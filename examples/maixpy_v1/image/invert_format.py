9a55ca24dfdfd5d6aea16911eaeca04dc4a94e9c
lxowalle <lxowalle@outlook.com>
2024-04-16 10:47:47 +0800

    * implement lab2rgb & rgb2lab

--- examples/maixpy_v1/image/invert_format.py
+++ examples/maixpy_v1/image/invert_format.py
@@ -18,5 +18,19 @@ img.to_rgb888()
 lcd.display(img)
 time.sleep(1)
 
+rgb=(100,200,30)
+lab=image.rgb_to_lab(rgb)
+print('rgb to lab, res:', lab)
+
+rgb=image.lab_to_rgb(lab)
+print('lab to rgb, res:', rgb)
+
+rgb=(10,20,30)
+gray=image.rgb_to_grayscale(rgb)
+print('rgb to gray, res:', gray)
+
+rgb=image.grayscale_to_rgb(gray)
+print('gray to rgb, res:', rgb)
+
 while True:
     time.sleep(1)
