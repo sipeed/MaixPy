13d065eea6eddebd79021d7c1e1bfee81f9f2578
lxowalle <lxowalle@outlook.com>
2024-05-11 18:37:20 +0800

    * add new examples

---
+++ examples/vision/video/video_encode.py
@@ -0,0 +1,19 @@
+from maix import camera, display, time, image, app, video
+
+cam = camera.Camera(640, 480, image.Format.FMT_YVU420SP)   # Manually set resolution, default is too large
+disp = display.Display()
+
+v = video.Video("/root/output.h265", cam.width(), cam.height(), capture = True)
+v.bind_camera(cam)
+
+record_ms = 5000
+start_ms = time.time_ms()
+while not app.need_exit():
+    v.encode()
+    img = v.capture()
+
+    disp.show(img)
+
+    if time.time_ms() - start_ms > record_ms:
+        v.finish()
+        app.set_exit_flag(True)
