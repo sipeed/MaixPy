ee2bac2dba74ad4bbb9da83836c25743f9c61af1
lxowalle <lxowalle@outlook.com>
2024-05-11 14:13:41 +0800

    * update audio example

---
+++ examples/vision/audio/audio_playback.py
@@ -0,0 +1,15 @@
+from maix import audio, time, app
+import maix
+
+p = audio.Player()
+print("sample_rate:{} format:{} channel:{}".format(p.sample_rate(), p.format(), p.channel()))
+
+with open('/root/output.pcm', 'rb') as f:
+    ctx = f.read()
+
+p.play(bytes(ctx))
+
+while not app.need_exit():
+    time.sleep_ms(10)
+
+print("play finish!")
