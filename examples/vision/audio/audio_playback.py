2b5d65e0788f0fd582c0c393908b22470f4f7e72
lxowalle <lxowalle@outlook.com>
2024-05-11 18:37:20 +0800

    * add new examples

--- examples/vision/audio/audio_playback.py
+++ examples/vision/audio/audio_playback.py
@@ -1,15 +1,14 @@
 from maix import audio, time, app
-import maix
-
+print(000)
 p = audio.Player()
 print("sample_rate:{} format:{} channel:{}".format(p.sample_rate(), p.format(), p.channel()))
 
 with open('/root/output.pcm', 'rb') as f:
     ctx = f.read()
-
+print(111)
 p.play(bytes(ctx))
-
+print(222)
 while not app.need_exit():
     time.sleep_ms(10)
-
+print(333)
 print("play finish!")
--- examples/vision/image_basic/image_ops.py
+++ examples/vision/image_basic/image_ops.py
@@ -8,3 +8,6 @@ img = image.Image(640, 480)
 img = img.resize(320, 240)
 img.save("test.jpg")
 
+jpg = img.to_jpeg(quality = 95)
+print(jpg.data_size())
+
