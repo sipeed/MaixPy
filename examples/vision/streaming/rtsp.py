6e239bfec903bd936d1746a30de9516838758b01
lxowalle <lxowalle@outlook.com>
2024-05-20 11:45:03 +0800

    * update rtsp doc

--- examples/vision/streaming/rtsp.py
+++ examples/vision/streaming/rtsp.py
@@ -1,7 +1,7 @@
 from maix import time, rtsp, camera, image
 
-server = rtsp.Rtsp()
 cam = camera.Camera(2560, 1440, image.Format.FMT_YVU420SP)
+server = rtsp.Rtsp()
 server.bind_camera(cam)
 server.start()
 
