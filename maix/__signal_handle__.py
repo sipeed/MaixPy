8bb3efab233953670d9b5b64876ceab8ffbbc627
lxowalle <lxowalle@outlook.com>
2024-05-30 18:45:24 +0800

    * optimize __signal_handle__

--- maix/__signal_handle__.py
+++ maix/__signal_handle__.py
@@ -1,9 +1,10 @@
-import signal
-import sys
+import signal, sys
+from . import _maix
 
 def signal_handle(signum, frame):
-    # _maix.app.set_exit_flag(True)
-    # signal.signal(signal.SIGINT, signal.SIG_DFL)
+    _maix.app.set_exit_flag(True)
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    # raise KeyboardInterrupt
     sys.exit(0)
 
 def register_signal_handle():
