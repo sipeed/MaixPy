4d2d2176a9062a3c2c9fa66fa1e790e280de6373
Mk16 <1041324852hzq@sina.com>
2024-06-07 09:56:43 +0800

    Added error message print and exit buttons. Updated documentation.

---
+++ projects/app_face_tracking/README_ZH.md
@@ -0,0 +1,89 @@
+# 人脸追踪
+
+## 简介
+
+基于 MaixCAM 和云台的人脸追踪程序。实际效果如下图所示：
+
+![](./assets/face_tracking1.jpg)
+
+![](./assets/face_tracking2.jpg)
+
+## 如何使用例程
+
+* 组装好您的云台和MaixCAM。
+
+* 修改 `main.py` 中的参数。
+
+  修改每个舵机使用的 MaixCAM 引脚，指定的引脚必须具备 PWM 功能。`servos.Servos` 会自行将该引脚配置为 PWM 功能。
+
+  ```python
+  ROLL_PWM_PIN_NAME = "A17"
+  PITCH_PWM_PIN_NAME = "A16"
+  ```
+
+  修改两个舵机的初始位置。
+
+  ```python
+  init_pitch = 80         # init position, value: [0, 100], means minimum angle to maxmum angle of servo
+  init_roll = 50          # 50 means middle
+  ```
+
+  修改两个舵机各自的活动范围的最小最大 PWM 占空比。请注意，某些云台结构中的舵机超出物理限制的最大活动范围时可能会造成不可预期的后果，请务必保证以下设定值对应的舵机运动范围内无阻碍。
+
+  ```python
+  PITCH_DUTY_MIN  = 3.5   # The minimum duty cycle corresponding to the range of motion of the y-axis servo.
+  PITCH_DUTY_MAX  = 9.5   # Maximum duty cycle corresponding to the y-axis servo motion range.
+  ROLL_DUTY_MIN   = 2.5   # Minimum duty cycle for x-axis servos.
+  ROLL_DUTY_MAX   = 12.5  # Maxmum duty cycle for x-axis servos.
+  ```
+
+  选择舵机的运动方向。
+
+  ```python
+  pitch_reverse = False                   # reverse out value direction
+  roll_reverse = True                     # reverse out value direction
+  ```
+
+* 最后执行代码即可。
+
+* 想要退出程序按左上角的按钮即可。
+
+  ![](./assets/exit.jpg)
+
+## 常见问题
+
+* 人脸跟踪效果不理想。
+
+  不同的云台使用的 PID 参数不尽相同，您可以通过调节 PID 值来使得追踪效果更丝滑。
+
+  ```python
+  pitch_pid = [0.3, 0.0001, 0.0018, 0]    # [P I D I_max]
+  roll_pid  = [0.3, 0.0001, 0.0018, 0]    # [P I D I_max]
+  ```
+
+* 在完成跟踪后，云台对着不动的人脸小幅度左右抖动一段时间。
+
+  通常可以通过调节 PID 来使得该影响尽可能小；但是无法避免云台物理结构带来的抖动。可以尝试调节死区来减小抖动。
+
+  ```python
+  target_ignore_limit = 0.08              
+  # when target error < target_err_range*target_ignore_limit , set target error to 0
+  ```
+
+* 显示屏显示或终端打印 `PIN: XXX does not exist`。
+
+  这是因为 MaixCAM 板子上引出的引脚中并不存在该引脚，请在 MaixCAM 上选择一个带 PWM 功能的引脚。
+
+* 显示屏显示或终端打印 `Pin XXX doesn't have PWM function`。
+
+  这是因为 MaixCAM 板子上引出的这个引脚没有 PWM 功能，请选择一个带 PWM 功能的引脚。
+
+  
+
+## 如何追踪其他物体
+
+* 在 `main.py` 中存在一个类 `Target`，该类用于自定义需要追踪的目标。
+
+* 在 `__init__` 中，请初始化您需要用到的对象，比如摄像头等。
+
+* 在 `__get_target()` 中，您需要计算出被追踪物体的中心点，如果帧中不存在被追踪物体，请返回 -1,-1 以确保程序在未找到目标时暂时不做动作。同时，您也需要在返回坐标点之前调用 `self.__exit_listener(img)` 和 `self.disp.show(img)` 确保程序能够与您正常的完成交互。
--- projects/app_face_tracking/main.py
+++ projects/app_face_tracking/main.py
@@ -1,8 +1,9 @@
 from face_tracking import servos
-from maix import image, camera, display, time, nn
+from maix import image, camera, display, time, nn, touchscreen
 
 ### model path
-MODEL = "/root/retinaface_models/retinaface_320.mud"
+MODEL = "/root/models/retinaface.mud"
+
 
 
 class Target:
@@ -15,7 +16,7 @@ class Target:
     """
     def __init__(self, out_range:float, ignore_limit:float, path:str):
         """Constructor
-        Initialization of the recognition model class or other classes needs to be implemented here.
+            Initialization of the recognition model class or other classes needs to be implemented here.
         """
         self.pitch = 0
         self.roll = 0
@@ -29,6 +30,46 @@ class Target:
         self.cam = camera.Camera(self.w, self.h)
         self.disp = display.Display()
         
+        ### The following section is used as an opt-out and normally you do not need to modify it.
+        self.ts = touchscreen.TouchScreen()
+        self.img_exit = image.load("./assets/exit.jpg").resize(40, 40)
+        self.img_exit_touch = image.load("./assets/exit_touch.jpg").resize(40, 40)
+        self.box = [0, 0, self.img_exit.width(), self.img_exit.height()]
+        self.need_exit = False
+        
+    def __check_touch_box(self, t, box, oft = 0):
+        """This method is used for exiting and you normally do not need to modify or call it.
+            You usually don't need to modify it.
+        """
+        if t[2] and t[0] + oft > box[0] and t[0] < box[0] + box[2] + oft and t[1] + oft > box[1] and t[1] < box[1] + box[3] + oft:
+            return True
+        else:
+            return False
+    
+    def __exit_listener(self, img):
+        """Exit case detection methods.
+            It also draws the Exit button in the upper left corner.
+            You usually don't need to modify it.
+
+        Args:
+            img (image.Image): The image that needs to be drawn.
+        """
+        t = self.ts.read()
+        if self.__check_touch_box(t, self.box, 20):
+            img.draw_image(self.box[0], self.box[1], self.img_exit_touch)
+            self.need_exit = True
+        else:
+            img.draw_image(self.box[0], self.box[1], self.img_exit)
+            
+    def is_need_exit(self):
+        """Queries whether the exit button has been pressed.
+            You usually don't need to modify it.
+
+        Returns:
+            bool: Returns true if the exit button has been pressed, false otherwise.
+        """
+        return self.need_exit
+        
     def __get_target(self):
         """Get the coordinate value of the target.
             The behavior of this function needs to be customized.
@@ -37,23 +78,25 @@ class Target:
                       If the target is found, return the coordinate values x,y of the center point of the target.
         """    
         ltime = time.time_ms()
-        img = self.cam.read()
-        objs = self.detector.detect(img, conf_th = 0.4, iou_th = 0.45)
-        for obj in objs:
+        img = self.cam.read()               # Reads an image frame.
+        objs = self.detector.detect(img, conf_th = 0.4, iou_th = 0.45)  # Recognition.
+        for obj in objs:                    # Find objects.
             img.draw_rect(obj.x, obj.y, obj.w, obj.h, image.COLOR_RED, 2)
-            cent_x = obj.x + round(obj.w/2)
-            cent_y = obj.y + round(obj.h/2)
+            cent_x = obj.x + round(obj.w/2) # Calculate the x-coordinate of the target center point.
+            cent_y = obj.y + round(obj.h/2) # Calculate the y-coordinate of the target center point.
             img.draw_rect(cent_x-1, cent_y-1, 2, 2, image.COLOR_GREEN)
             rtime = time.time_ms()
-            print(f"find target used time:{round(rtime-ltime,2)}ms")
-            self.disp.show(img)
-            return cent_x, cent_y
+            # print(f"find target used time:{round(rtime-ltime,2)}ms")
+            self.__exit_listener(img)       # Queries whether the Exit button was pressed.
+            self.disp.show(img)             # Display this image.
+            return cent_x, cent_y           # Return (x, y)
+        self.__exit_listener(img)           
         self.disp.show(img)
-        return -1, -1
+        return -1, -1                       # Target not found. Return (-1, -1)
 
     def get_target_err(self):
         """Obtain the error value between the target and the center point.
-        Normally, you don't have to modify this class method.
+            You usually don't need to modify it.
 
         Returns:
             int, int: y-axis error value, x-axis error value.
@@ -71,12 +114,14 @@ class Target:
           
 
 if __name__ == '__main__':
-    init_pitch = 80         # init position, value: [0, 100], means minimum angle to maxmum angle of servo
-    init_roll = 50          # 50 means middle
-    PITCH_DUTY_MIN  = 3.5   # The minimum duty cycle corresponding to the range of motion of the y-axis servo.
-    PITCH_DUTY_MAX  = 9.5   # Maximum duty cycle corresponding to the y-axis servo motion range.
-    ROLL_DUTY_MIN   = 2.5   # Minimum duty cycle for x-axis servos.
-    ROLL_DUTY_MAX   = 12.5  # Maxmum duty cycle for x-axis servos.
+    ROLL_PWM_PIN_NAME = "A17"   
+    PITCH_PWM_PIN_NAME = "A16"
+    init_pitch = 80             # init position, value: [0, 100], means minimum angle to maxmum angle of servo
+    init_roll = 50              # 50 means middle
+    PITCH_DUTY_MIN  = 3.5       # The minimum duty cycle corresponding to the range of motion of the y-axis servo.
+    PITCH_DUTY_MAX  = 9.5       # Maximum duty cycle corresponding to the y-axis servo motion range.
+    ROLL_DUTY_MIN   = 2.5       # Minimum duty cycle for x-axis servos.
+    ROLL_DUTY_MAX   = 12.5      # Maxmum duty cycle for x-axis servos.
     
     pitch_pid = [0.3, 0.0001, 0.0018, 0]    # [P I D I_max]
     roll_pid  = [0.3, 0.0001, 0.0018, 0]    # [P I D I_max]
@@ -86,34 +131,47 @@ if __name__ == '__main__':
     roll_reverse = True                     # reverse out value direction
     
     target = Target(target_err_range, target_ignore_limit, MODEL)
-    roll = servos.Servos("A17", init_roll, ROLL_DUTY_MIN, ROLL_DUTY_MAX)
-    pitch = servos.Servos("A16", init_pitch, PITCH_DUTY_MIN, PITCH_DUTY_MAX)
+    try:
+        roll = servos.Servos(ROLL_PWM_PIN_NAME, init_roll, ROLL_DUTY_MIN, ROLL_DUTY_MAX)
+        pitch = servos.Servos(PITCH_PWM_PIN_NAME, init_pitch, PITCH_DUTY_MIN, PITCH_DUTY_MAX)
+    except RuntimeError as e:
+        print(f"!!!!!!!!!!!!!!!! ERROR: {e} !!!!!!!!!!!!!!!!!!!!!!")
+        wait_time_s = 10
+        while wait_time_s:
+            eimg = image.Image(target.w, target.h)
+            eimg.draw_string(10, 10, "Error: "+str(e)+
+                             f".   This program will exit after {wait_time_s}s.")
+            target.disp.show(eimg)
+            time.sleep(1)
+            wait_time_s -= 1
+        exit(-1)
+    
     pid_pitch = servos.PID(p=pitch_pid[0], i=pitch_pid[1], d=pitch_pid[2], imax=pitch_pid[3])
     pid_roll = servos.PID(p=roll_pid[0], i=roll_pid[1], d=roll_pid[2], imax=roll_pid[3])
     gimbal = servos.Gimbal(pitch, pid_pitch, roll, pid_roll)
     
-    target_pitch = init_pitch
-    target_roll = init_roll
     total_uesd_time = 0
     total_fps = 0
     t0 = time.time_ms()
-    while True:
+    while not target.is_need_exit():
         ltime = time.time_ms()
+        
         # get target error
         err_pitch, err_roll = target.get_target_err()
-        # print(f"err_pitch:{round(err_pitch,2)}, err_roll:{round(err_roll,2)}")
-        
         # interval limit to >= 10ms
         if time.time_ms() - t0 < 10:
             continue
         t0 = time.time_ms()
         # run
         gimbal.run(err_pitch, err_roll, pitch_reverse = pitch_reverse, roll_reverse=roll_reverse)
+        
+        # Calculate FPS.
         rtime = time.time_ms()
         utime = rtime-ltime
         total_uesd_time += utime
         total_fps += 1
         print(f"used time:{utime}ms, fps:{round(1000/(utime),2)}, avg_fps:{round(total_fps*1000/total_uesd_time, 2)}")
+        
     
     
     
