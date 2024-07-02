a467d65a11bbd8fac5d19d926e18064e742d8d8d
lxowalle <lxowalle@outlook.com>
2024-07-02 18:05:15 +0800

    * add rtsp app

---
+++ projects/app_rtsp/main.py
@@ -0,0 +1,56 @@
+from maix import app, rtsp, camera, image, display, touchscreen
+
+# init camera
+cam = camera.Camera(640, 480, image.Format.FMT_YVU420SP)
+cam2 = cam.add_channel(552, 368)
+
+# init display
+disp = display.Display()
+ts = touchscreen.TouchScreen()
+
+# init rtsp server
+server = rtsp.Rtsp()
+server.bind_camera(cam)
+server.start()
+urls = server.get_urls()
+print(server.get_urls())
+
+img_exit = image.load("./assets/exit.jpg").resize(50, 50)
+img_exit_touch = image.load("./assets/exit_touch.jpg").resize(50, 50)
+img_eye_open = image.load("./assets/img_eye_open.png").resize(50, 50)
+img_eye_close = image.load("./assets/img_eye_close.png").resize(50, 50)
+
+def touch_box(t, box, oft = 0):
+    if t[2] and t[0] + oft > box[0] and t[0] < box[0] + box[2] + oft and t[1] + oft > box[1] and t[1] < box[1] + box[3] + oft:
+        return True
+    else:
+        return False
+
+need_exit = False
+show_urls = False
+while not app.need_exit():
+    img = cam2.read()
+    t = ts.read()
+
+    box = [20, 15, img_exit.width(), img_exit.height()]
+    if touch_box(t, box, 20):
+        img.draw_image(box[0], box[1], img_exit_touch)
+        need_exit = True
+    else:
+        img.draw_image(box[0], box[1], img_exit)
+
+    box = [img.width() - img_eye_open.width() - 20, 15, img_eye_open.width(), img_eye_open.height()]
+    if touch_box(t, box, 20):        
+        show_urls = not show_urls
+
+    if show_urls:
+        img.draw_image(box[0], box[1], img_eye_open)
+        for i, url in enumerate(urls):
+            img.draw_string(100, i * 20 + 50, url)
+    else:
+        img.draw_image(box[0], box[1], img_eye_close)
+
+    disp.show(img)
+
+    if need_exit:
+        break
