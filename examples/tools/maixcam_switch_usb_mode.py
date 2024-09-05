9d3f41d69481da05ff4bb1a8b97a25d477a87c98
Neucrack <CZD666666@gmail.com>
2024-09-05 17:29:02 +0800

    release for v4.5.0

--- examples/tools/maixcam_switch_usb_mode.py
+++ examples/tools/maixcam_switch_usb_mode.py
@@ -2,14 +2,6 @@ import os
 
 cmd_restart = "/etc/init.d/S03usbdev stop &&/etc/init.d/S03usbdev start"
 
-device_list = [
-    ["/boot/usb.ncm", True],
-    ["/boot/usb.rndis", True],
-    ["/boot/usb.keyboard", False],
-    ["/boot/usb.mouse", False],
-    ["/boot/usb.touchpad", False]
-]
-
 def usb_devive(device_list):
     if os.path.exists("/boot/usb.host"):
         os.remove("/boot/usb.host")
@@ -17,12 +9,13 @@ def usb_devive(device_list):
         pass
 
     for device in device_list:
+        dev_path = f"/boot/usb.{device[0]}"
         if device[1]:
-            with open(device[0], "w") as f:
+            with open(dev_path, "w") as f:
                 pass
         else:
-            if os.path.exists(device[0]):
-                os.remove(device[0])
+            if os.path.exists(dev_path):
+                os.remove(dev_path)
 
     ret = os.system(cmd_restart)
     if ret != 0:
@@ -50,6 +43,15 @@ if __name__ == "__main__":
 
     mode = "device" # MaixCAM as device
     # mode = "host"     # MaixCAM as host, you can plugin devices to MaixCAM's USB like USB camera.
+
+    # device mode functions, set your need to True and others to False. only support 4 devices for MaixCAM.
+    device_list = [
+        ["ncm", True],        # NCM virtual network card, faster trhan RNDIS but need manual install driver on windows <=10.
+        ["rndis", True],      # RNDIS virtual network card, compatible for Windows and Linux but MacOS not suppot.
+        ["keyboard", False],  # Simulate USB HID keyboard input, then use maix.hid module.
+        ["mouse", False],     # Simulate USB HID mouse input, then use maix.hid module.
+        ["touchpad", False]   # Simulate USB HID touchpad input, then use maix.hid module.
+    ]
 
     if mode == "device":
         usb_devive(device_list)
