a51f8ae4c43069d8a1ef18bacd9d1884f3bd6262
Neucrack <CZD666666@gmail.com>
2024-09-26 19:16:43 +0800

    update version to 4.6.0

--- maix/version.py
+++ maix/version.py
@@ -2,7 +2,7 @@
 # Versions should comply with PEP440: https://peps.python.org/pep-0440/
 
 version_major = 4
-version_minor = 5
-version_patch = 1
+version_minor = 6
+version_patch = 0
 
 __version__ = "{}.{}.{}".format(version_major, version_minor, version_patch)
