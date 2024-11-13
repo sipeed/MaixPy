2265ac3f8cf74dfe11f83e793d234d4a92465727
lxowalle <lxowalle@outlook.com>
2024-11-13 18:01:55 +0800

    * update hid_keyboard example

--- examples/peripheral/usb/hid_keyboard.py
+++ examples/peripheral/usb/hid_keyboard.py
@@ -5,15 +5,21 @@ try:
     keyboard = hid.Hid(hid.DeviceType.DEVICE_KEYBOARD)
 except Exception as e:
     print(e)
-    print('The HID evice is not enabled')
+    print('The HID device is not enabled')
     print('Try: Use the application Settings -> USB Settings -> HID Keyboard, then click Confirm, and restart the system.')
     print('You can also use examples/tools/maixcam_switch_usb_mode.py to enable the HID Keyboard, then restart the system.')
     exit(0)
 
-# Refer to the "Universal Serial Bus HID Usage Tables" section of the official documentation(https://www.usb.org).
-keys = [21, 22, 23, 24, 25, 0]    # [r, s, t, u, v, 0], 0 means release. 
-
-for key in keys:
+def press(keyboard, key):
     keyboard.write([0, 0, key, 0, 0, 0, 0, 0])
+    time.sleep_ms(50)
+    keyboard.write([0, 0, 0, 0, 0, 0, 0, 0])        #  key=0, means release
+
+# Refer to the "Universal Serial Bus HID Usage Tables" section of the official documentation(https://www.usb.org).
+press(keyboard, 21)                 # press 'r'
+press(keyboard, 22)                 # press 's'
+press(keyboard, 23)                 # press 't'
+press(keyboard, 24)                 # press 'u'
+press(keyboard, 25)                 # press 'v'
 
 print('OK')
