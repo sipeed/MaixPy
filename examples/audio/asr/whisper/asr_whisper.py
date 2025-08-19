28e15da6604b0efd0863e6c40db018bef0963287
lxowalle <lxowalle@outlook.com>
2025-08-19 18:35:02 +0800

    * update whisper example

--- examples/audio/asr/whisper/asr_whisper.py
+++ examples/audio/asr/whisper/asr_whisper.py
@@ -1,10 +1,24 @@
-from maix import nn
+from maix import nn, audio
 
 # Only MaixCAM2 supports this model.
 whisper = nn.Whisper(model="/root/models/whisper-base/whisper-base.mud")
 
-file_name = "/maixapp/share/audio/demo.wav"
+use_default_file = True
 
-res = whisper.transcribe(file_name)
+if use_default_file:
+    file_name = "/maixapp/share/audio/demo.wav"
+    print('Use default file:', file_name)
+    print('Start transcribing..')
+    res = whisper.transcribe(file_name)
+    print(res)
+else:
+    file_name = "/root/audio.wav"
+    recorder = audio.Recorder(file_name, sample_rate=16000, channel=1)
+    recorder.volume(80)
+    print('Recording for 3 seconds..')
+    recorder.record(3 * 1000)
+    recorder.finish()
 
-print(res)
+    print('Start transcribing..')
+    res = whisper.transcribe(file_name)
+    print(res)
