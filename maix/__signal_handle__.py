75abff7816333779241ae9db49235d0e2209821c
Neucrack <CZD666666@gmail.com>
2025-09-17 19:52:53 +0800

    optimize app exit to wait for 2 seconds before force exit

--- maix/__signal_handle__.py
+++ maix/__signal_handle__.py
@@ -1,11 +1,21 @@
-import signal, sys
+import signal, sys, threading
 from . import _maix
+
+force_exit_timeout = 2
+
+def force_exit():
+    print("Force exit now")
+    sys.exit(1)
 
 def signal_handle(signum, frame):
     _maix.app.set_exit_flag(True)
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    # raise KeyboardInterrupt
-    sys.exit(0)
+
+    # set exit flag and wait force_exit_timeout seconds, then force exit
+    t = threading.Timer(force_exit_timeout, force_exit)
+    t.daemon = True
+    t.start()
+
 
 def register_signal_handle():
     signal.signal(signal.SIGINT, signal_handle)
