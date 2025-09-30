fd307c78ef34769ea5a63fc1d3a3c3435e9404ae
Neucrack <CZD666666@gmail.com>
2025-09-30 16:16:20 +0800

    bump version to v4.12.0

--- maix/version.py
+++ maix/version.py
@@ -2,7 +2,7 @@
 # Versions should comply with PEP440: https://peps.python.org/pep-0440/
 
 version_major = 4
-version_minor = 11
-version_patch = 9
+version_minor = 12
+version_patch = 0
 
 __version__ = "{}.{}.{}".format(version_major, version_minor, version_patch)
