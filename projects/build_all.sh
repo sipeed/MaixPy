8e8eb548b483753650f87d76510ac9485af6e66b
lxowalle <lxowalle@outlook.com>
2025-12-15 15:50:20 +0800

    * revert thermal256 camera, add thermal night vision

--- projects/build_all.sh
+++ projects/build_all.sh
@@ -7,7 +7,8 @@ set -e
 # 定义不同平台的黑名单
 
 blacklist_linux=()
-blacklist_maixcam=("app_yoloworld" "app_vlm" "app_mono_depth_estimation" "app_chat" "app_speech" "app_thermal256_camera" "app_image_generation")
+blacklist_maixcam=("app_yoloworld" "app_vlm" "app_mono_depth_estimation" "app_chat"
+ "app_speech" "app_thermal256_camera" "app_image_generation" "app_thermal256_nightvision")
 blacklist_maixcam2=()
 
 #############################################
